b98d4582718148f566bc78da5649adb6af797949
volea <artem@pron.vip>
2025-07-18 12:33:03 +0300

    *

--- index.php
+++ index.php
@@ -20,6 +20,14 @@
     <script type="module" src="scripts.js"></script>
     <script type="module" src="home-flying_text.js"></script>
     <script type="module" src="parallax_img.js"></script>
+    <style>
+        .text-green-700 {
+            color: #39ff14;
+        }
+        .text-gray-400 {
+            color: #c6cdcf;
+        }
+    </style>
 
 </head>
 <body class="dark:bg-[#151515] bg-[#fcfcfc] text-black dark:text-white font-lato transition-colors">
@@ -208,7 +216,7 @@
                 </li>
             </ul>
         </div>
-        <button type="button" data-dial-toggle="speed-dial-menu-dropdown-square" aria-controls="speed-dial-menu-dropdown-square" aria-expanded="false" class="flex items-center justify-center ml-auto text-white bg-blue-700 rounded-lg w-14 h-14 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
+        <button type="button" data-dial-toggle="speed-dial-menu-dropdown-square" aria-controls="speed-dial-menu-dropdown-square" aria-expanded="false" class="flex items-center justify-center ml-auto text-white bg-green-700 rounded-lg w-14 h-14 hover:bg-green-800 dark:bg-green-600 dark:hover:bg-green-700 focus:ring-4 focus:ring-green-300 focus:outline-none dark:focus:ring-green-800">
             <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
                 <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
             </svg>
